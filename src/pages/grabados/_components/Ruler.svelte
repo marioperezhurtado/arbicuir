c46139d482b6436907724a140b0d4be7855b638e
Mario Perez Hurtado <marioph10@gmail.com>
2023-11-23 17:06:26 +0100

    chore: optimize images

--- src/pages/grabados/_components/Ruler.svelte
+++ src/pages/grabados/_components/Ruler.svelte
@@ -3,16 +3,15 @@
 </script>
 
 <div
-  class="h-full bg-ac-dark-2/60 absolute bottom-0 flex flex-col-reverse
-      justify-evenly pl-5 pr-1"
+  class="flex absolute bottom-0 flex-col-reverse justify-evenly pr-1 pl-5 h-full bg-ac-dark-2/60"
 >
   {#each { length: RULER_HEIGHT } as _, i}
-    <div class="flex relative gap-1.5 items-center justify-end">
+    <div class="flex relative gap-1.5 justify-end items-center">
       {#if (i + 1) % 2 === 0}
         <span class="absolute right-9 text-xs">{(i + 1) / 2}</span>
       {/if}
       <div
-        class="h-[3px] mb-0.5 bg-ac-light/60 rounded-md"
+        class="mb-0.5 rounded-md h-[3px] bg-ac-light/60"
         class:w-4={i % 2 === 0}
         class:w-8={i % 2 !== 0}
       />
