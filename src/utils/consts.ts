6554e600b6b79c69d6e88ffab54a27a5d0c6e780
Mario Perez Hurtado <marioph10@gmail.com>
2023-11-24 11:26:24 +0100

    style: about us page changes

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,6 @@
 export const ADDRESS =
-  'Pol. ind. tres hermanas, fase 2 Calle fábrica de la luz, 28, 03680 Aspe, Alicante'
-export const PHONE = '+34 966675525'
-export const EMAIL = 'info@arbicuir.es'
-export const SITE = 'https://arbicuir.es'
+  'Pol. ind. tres hermanas, fase 2 Calle fábrica de la luz, 28, 03680 Aspe, Alicante';
+export const PHONE = '+34 966675525';
+export const EMAIL = 'info@arbicuir.es';
+export const SITE = 'https://arbicuir.es';
+export const EXPERIENCE = new Date().getFullYear() - 1995;
