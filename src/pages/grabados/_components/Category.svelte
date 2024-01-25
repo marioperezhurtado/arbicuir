f6015c4c1b81b5e5575909f21f6aa63a7c4b1ed0
Mario Perez Hurtado <marioph10@gmail.com>
2024-01-25 18:14:19 +0100

    feat: add search bar

---
+++ src/pages/grabados/_components/Category.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let href: string;
+  export let title: string;
+  export let selected: boolean;
+</script>
+
+<li>
+  <a
+    {href}
+    class={`block px-3 pt-2 pb-1 transition rounded-md shadow-md bg-ac-dark-2
+    hover:brightness-125 ${selected ? "bg-ac-light-2 text-ac-dark" : ""}`}
+  >
+    {title}
+  </a>
+</li>
