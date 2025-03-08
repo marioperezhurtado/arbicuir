70341bce749a4b5cd31cc1d29728a5bb13fc8127
Mario Perez Hurtado <marioph10@gmail.com>
2025-03-08 21:59:32 +0100

    feat: add hover zoom effect on engraving images

--- src/pages/grabados/_components/ExpandedEngraving.svelte
+++ src/pages/grabados/_components/ExpandedEngraving.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
   import { fade, scale } from "svelte/transition";
   import Ruler from "./Ruler.svelte";
+  import HoverZoom from "./HoverZoom.svelte";
   import type { Engraving } from "./types.ts";
 
   export let engraving: Engraving;
   export let onClose: () => void;
+
+  let imgElement: HTMLImageElement;
 </script>
 
-<div
-  class="flex fixed top-0 left-0 z-30 justify-center items-center w-screen h-screen"
->
+<div class="fixed top-0 left-0 z-30 flex items-center justify-center w-screen h-screen">
   <button
     in:fade={{ duration: 200 }}
     out:fade={{ duration: 100 }}
@@ -18,12 +19,13 @@
   />
   <div
     in:scale={{ start: 0.8, duration: 300 }}
-    class="overflow-hidden relative z-30 mx-auto rounded-md shadow-md w-fit"
+    class="relative z-30 mx-auto overflow-hidden rounded-md shadow-md w-fit"
   >
     <img
+      bind:this={imgElement}
       src={engraving.url}
       alt={`Grabado ${engraving.title} de Arbicuir, S.L.`}
-      class="object-cover h-[70vh] max-h-[704px] bg-ac-dark-2"
+      class="object-cover h-[70vh] md:h-[80vh] bg-ac-dark-2 cursor-none"
     />
     <p
       class="absolute bottom-10 left-1/2 px-3 pt-2.5 pb-1.5 text-xl rounded-md -translate-x-1/2 font-display bg-ac-dark-2/60 text-ac-light w-fit"
@@ -32,10 +34,12 @@
     </p>
     <button
       on:click={onClose}
-      class="absolute top-2 right-2 transition hover:brightness-125"
+      class="absolute transition top-2 right-2 hover:brightness-125"
     >
       <img src="/icons/close.svg" alt="Close" class="w-9 h-9" />
     </button>
+
     <Ruler />
+    <HoverZoom {imgElement} {engraving} />
   </div>
 </div>
--- src/pages/grabados/_components/Ruler.svelte
+++ src/pages/grabados/_components/Ruler.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <div
-  class="flex absolute bottom-0 flex-col-reverse justify-evenly pr-1 pl-5 h-full bg-ac-dark-2/60"
+  class="absolute bottom-0 flex flex-col-reverse h-full pl-5 pr-1 justify-evenly bg-ac-dark-2/60"
 >
   {#each { length: RULER_HEIGHT } as _, i}
     <div class="flex relative gap-1.5 justify-end items-center">
       {#if (i + 1) % 2 === 0}
-        <span class="absolute right-9 text-xs">{(i + 1) / 2}</span>
+        <span class="absolute text-xs right-9">{(i + 1) / 2}</span>
       {/if}
       <div
         class="mb-0.5 rounded-md h-[3px] bg-ac-light/60"
